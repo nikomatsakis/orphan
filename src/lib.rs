eb2c451a6def33182ce7042fd2c8449a7bdab67d
Niko Matsakis <niko@alum.mit.edu>
2014-12-11 06:30:42 -0500

    Add some more test cases, tweak rule to something that seems to work, but I haven't added in all the test cases yet.

--- src/lib.rs
+++ src/lib.rs
@@ -24,8 +24,7 @@ fn orphan(krate: Crate, types: &[Type]) -> bool {
 
 fn not_orphan(krate: Crate, types: &[Type]) -> bool {
     let result = krate == Local || {
-        types.iter().any(|t| type_local(t)) &&
-            types.iter().all(|t| *t != Parameter)
+        types.iter().all(|t| type_local(t))
     };
 
     debug!("not_orphan({},{}) = {}",
@@ -37,7 +36,7 @@ fn not_orphan(krate: Crate, types: &[Type]) -> bool {
 fn type_local(ty: &Type) -> bool {
     let result = match *ty {
         Concrete(krate, ref types) => not_orphan(krate, types[]),
-        Parameter => false
+        Parameter => false,
     };
 
     debug!("type_local({}) = {}",
@@ -59,16 +58,34 @@ fn type_parameter() {
 }
 
 #[test]
-fn conflict() {
+fn overlapping_pairs() {
     /*! `impl<T> Show for Pair<Option<T>, Option<Foo>>` -- Bad */
 
     // Bad because another crate could do:
     // impl<T> Show for Pair<Option<Bar>, Option<T>>
 
+    assert!(orphan(Remote,
+                   &[Concrete(Remote, // Pair
+                              vec!(Concrete(Remote, // Option
+                                            vec!(Parameter)), // T
+                                   Concrete(Local, // Foo
+                                            vec!())))]));
+}
+
+#[test]
+fn bigint_int() {
+    /*! `impl Add<Foo> for int` -- OK */
+
     assert!(not_orphan(Remote,
-                       &[Concrete(Remote, // Pair
-                                  vec!(Concrete(Remote, // Option
-                                                vec!(Parameter)), // T
-                                       Concrete(Local, // Foo
-                                                vec!())))]));
+                       &[Concrete(Local, vec!()),
+                         Concrete(Remote, vec!())]));
+}
+
+#[test]
+fn bigint_param() {
+    /*! `impl Add<Foo> for T` -- not OK */
+
+    assert!(orphan(Remote,
+                   &[Concrete(Local, vec!()),
+                     Parameter]));
 }
